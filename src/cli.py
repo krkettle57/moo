e79f435bcb8fec1a831524a30b29e2cd896f8b25
krkettle57 <krkettle57@gmail.com>
2021-10-05 07:53:40 +0900

    fix not input bug

--- src/cli.py
+++ src/cli.py
@@ -6,23 +6,24 @@ from store import MOOFileStore, MOOJSONStore
 
 
 class CLIMessage(Enum):
-    MOO_ALREADY_STARTED = "現在進行中のMOOが存在します。\n新しくMOOを開始しますか？[y/n]"
-    CANCEL_NEW_MOO = "新しいMOOの開始を中止しました\n"
+    MOO_ALREADY_STARTED = "現在進行中のMOOが存在します。\n新しくMOOを開始しますか？[y/n]: "
+    CANCEL_NEW_MOO = "新しいMOOの開始を中止しました"
+    START_MOO = "MOOを開始します！"
 
 
 @dataclass
 class MOOCLIHandler:
     store: MOOFileStore = MOOJSONStore()
 
-    def start(self, called: str, target_length: TargetLengthOption = 3) -> str:
+    def start(self, target_length: TargetLengthOption = 3) -> str:
         if self.store.exists():
-            yn = CLIMessage.MOO_ALREADY_STARTED.value
+            yn = input(CLIMessage.MOO_ALREADY_STARTED.value)
             if yn.lower().strip() != "y":
                 return CLIMessage.CANCEL_NEW_MOO.value
 
         moo = MOO(target_length)
         self.store.save(moo)
-        return self.turn(called)
+        return CLIMessage.START_MOO.value
 
     def turn(self, called: str) -> str:
         # 管理ファイルからMOOをload
