3a96d805d7a2fc81c3c3b9c95850027c4b248098
krkettle57 <krkettle57@gmail.com>
2021-10-04 21:05:00 +0900

    ignore mypy linting on tests files

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,19 +2,19 @@ import pytest
 from model import MOO
 
 
-class TestMOO:
-    def test_target_default(self) -> None:
+class TestMOOConstructor:
+    def test_target_default(self):
         moo = MOO()
         assert len(moo.target) == 3
         for num in moo.target:
             assert num in [i for i in range(10)]
 
-    def test_target_length4(self) -> None:
+    def test_target_length4(self):
         moo = MOO(target_length=4)
         assert len(moo.target) == 4
         for num in moo.target:
             assert num in [i for i in range(10)]
 
-    def test_target_length10(self) -> None:
+    def test_target_length10(self):
         with pytest.raises(ValueError):
             MOO(target_length=10)
