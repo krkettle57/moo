cb4b309171472299c72be368240f49e2e5783523
krkettle57 <krkettle57@gmail.com>
2021-10-05 08:41:21 +0900

    add giveup

--- src/cli.py
+++ src/cli.py
@@ -7,8 +7,9 @@ from store import MOOFileStore, MOOJSONStore
 
 class CLIMessage(Enum):
     MOO_ALREADY_STARTED = "現在進行中のMOOが存在します。\n新しくMOOを開始しますか？[y/n]: "
-    CANCEL_NEW_MOO = "新しいMOOの開始を中止しました"
+    CANCEL_NEW_MOO = "新しいMOOの開始を中止しました。"
     START_MOO = "MOOを開始します！"
+    GIVEUP_MOO = "MOOを終了します。ターゲットは{target}でした。"
 
 
 @dataclass
@@ -16,7 +17,7 @@ class MOOCLIHandler:
     store: MOOFileStore = MOOJSONStore()
 
     def start(self, target_length: TargetLengthOption = 3) -> str:
-        if self.store.exists():
+        if self.store.exists() and self.store.load().onPlay:
             yn = input(CLIMessage.MOO_ALREADY_STARTED.value)
             if yn.lower().strip() != "y":
                 return CLIMessage.CANCEL_NEW_MOO.value
@@ -24,6 +25,13 @@ class MOOCLIHandler:
         moo = MOO(target_length)
         self.store.save(moo)
         return CLIMessage.START_MOO.value
+
+    def giveup(self) -> str:
+        moo = self.store.load()
+        moo.finish()
+        self.store.save(moo)
+        target = "".join([str(i) for i in moo.target])
+        return CLIMessage.GIVEUP_MOO.value.format(target=target)
 
     def turn(self, called: str) -> str:
         # 管理ファイルからMOOをload
