87725b273796ac950305db8f55fe6a9794e9a47c
krkettle57 <krkettle57@gmail.com>
2021-10-05 11:08:14 +0900

    add Target & Called model

--- src/model.py
+++ src/model.py
@@ -2,16 +2,58 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from random import randint
-from typing import List, Literal, get_args
+from typing import List, Literal, Tuple, get_args
 
 TargetLengthOption = Literal[3, 4, 5]
 
 
 @dataclass
 class CallResultSet:
-    called: str
+    called: Called
     num_eat: int
     num_bite: int
+
+
+@dataclass
+class Target:
+    target: str
+
+    @classmethod
+    def generate(self, length: TargetLengthOption) -> Target:
+        if length not in get_args(TargetLengthOption):
+            raise ValueError(f"'length' should be in {get_args(TargetLengthOption)}.")
+
+        target = "".join([str(randint(0, 9)) for _ in range(length)])
+        return Target(target)
+
+    @property
+    def length(self) -> int:
+        return len(self.target)
+
+    def as_list(self) -> List[str]:
+        return list(self.target)
+
+
+@dataclass
+class Called:
+    called: str
+    target: Target
+
+    def __post_init__(self) -> None:
+        if len(self.called) != self.target.length:
+            raise ValueError(f"Length of 'called' must be {self.target.length}")
+
+    def as_list(self) -> List[str]:
+        return list(self.called)
+
+    def get_eat_bite_num(self) -> Tuple[int, int]:
+        num_eat = sum([td == cd for td, cd in zip(self.target.as_list(), self.called)])
+        num_bite = len(set(self.target.as_list()) & set(self.called)) - num_eat
+        return num_eat, num_bite
+
+    def get_result_set(self) -> CallResultSet:
+        num_eat, num_bite = self.get_eat_bite_num()
+        return CallResultSet(self, num_eat, num_bite)
 
 
 @dataclass
