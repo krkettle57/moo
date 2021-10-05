ef29dca24a71609301e92c3273faf97cb3e0fe14
krkettle57 <krkettle57@gmail.com>
2021-10-05 14:30:36 +0900

    split cli & viewer

--- src/cli.py
+++ src/cli.py
@@ -1,38 +1,9 @@
 from dataclasses import dataclass, field
-from typing import List, Union
+from typing import Union
 
-from model import MOO, CallResultSet, Target, TargetLengthOption
+from model import MOO, Target, TargetLengthOption
 from store import MOOFileStore, MOOJSONStore
-
-
-class MOOCLIViewer:
-    def start_verify(self, yes: str = "y", no: str = "n") -> str:
-        return f"進行中のMOOが存在します。\n新しくMOOを開始しますか？[{yes}/{no}]: "
-
-    def start_done(self) -> str:
-        return "MOOを開始しました"
-
-    def start_cancel(self) -> str:
-        return "新しいMOOの開始を中止しました。"
-
-    def giveup(self, target: Target) -> str:
-        return f"MOOを終了します。ターゲットは{target.target}でした。"
-
-    def clear(self, moo: MOO) -> str:
-        return f"クリア！ターゲット: {moo.target.target}, コール数: {len(moo.called_results)}"
-
-    def called_result(self, result: CallResultSet) -> str:
-        return f"コール: {result.called.called}, {result.num_eat}-EAT, {result.num_bite}-BITE"
-
-    def history(self, called_results: List[CallResultSet]) -> str:
-        hist = [self.called_result(result) for result in called_results]
-        return "\n".join(["[Called History]"] + hist)
-
-    def no_moo_on_play(self) -> str:
-        return "進行中のMOOが存在しません。 startコマンドで開始して下さい。"
-
-    def no_moo_started(self) -> str:
-        return "MOOのプレイ記録が存在しません。startコマンドで開始して下さい。"
+from viewer import MOOCLIViewer
 
 
 @dataclass
---
+++ src/viewer.py
@@ -0,0 +1,33 @@
+from typing import List
+
+from model import MOO, CallResultSet, Target
+
+
+class MOOCLIViewer:
+    def start_verify(self, yes: str = "y", no: str = "n") -> str:
+        return f"進行中のMOOが存在します。\n新しくMOOを開始しますか？[{yes}/{no}]: "
+
+    def start_done(self) -> str:
+        return "MOOを開始しました"
+
+    def start_cancel(self) -> str:
+        return "新しいMOOの開始を中止しました。"
+
+    def giveup(self, target: Target) -> str:
+        return f"MOOを終了します。ターゲットは{target.target}でした。"
+
+    def clear(self, moo: MOO) -> str:
+        return f"クリア！ターゲット: {moo.target.target}, コール数: {len(moo.called_results)}"
+
+    def called_result(self, result: CallResultSet) -> str:
+        return f"コール: {result.called.called}, {result.num_eat}-EAT, {result.num_bite}-BITE"
+
+    def history(self, called_results: List[CallResultSet]) -> str:
+        hist = [self.called_result(result) for result in called_results]
+        return "\n".join(["[Called History]"] + hist)
+
+    def no_moo_on_play(self) -> str:
+        return "進行中のMOOが存在しません。 startコマンドで開始して下さい。"
+
+    def no_moo_started(self) -> str:
+        return "MOOのプレイ記録が存在しません。startコマンドで開始して下さい。"
