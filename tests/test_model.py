f83dd4b4d44d71d1a29b5bfa010e5919cfa452f9
krkettle57 <krkettle57@gmail.com>
2021-10-05 11:46:26 +0900

    update pytest to pass

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,75 +1,73 @@
 import pytest
-from model import MOO
+from model import MOO, Called, Target
 
 
-class TestMOOConstructor:
+class TargetConstructor:
     def test_target_length3(self):
-        moo = MOO(3)
-        assert len(moo.target) == 3
-        for num in moo.target:
-            assert num in [i for i in range(10)]
+        target = Target.generate(3)
+        assert len(target.target) == 3
+        assert target.target.isdigit()
 
     def test_target_length4(self):
-        moo = MOO(4)
-        assert len(moo.target) == 4
-        for num in moo.target:
-            assert num in [i for i in range(10)]
+        target = Target.generate(4)
+        assert len(target.target) == 4
+        assert target.target.isdigit()
 
     def test_target_length10(self):
         with pytest.raises(ValueError):
-            MOO(target_length=10)
+            Target.generate(10)
 
 
 @pytest.fixture()
-def fixed_moo():
-    moo = MOO(3)
-    moo.target = [1, 2, 3]
+def fixed_target():
+    target = Target.generate(3)
+    target.target = "123"
 
-    yield moo
+    yield target
 
 
-class TestMOOCall:
-    def test_call_3eat(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([1, 2, 3])
+class TestCalled:
+    def test_call_3eat(self, fixed_target) -> None:
+        called_result = Called("123", fixed_target).get_result_set()
         assert called_result.num_eat == 3
         assert called_result.num_bite == 0
 
-    def test_call_2eat0bite(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([1, 2, 4])
+    def test_call_2eat0bite(self, fixed_target) -> None:
+        called_result = Called("124", fixed_target).get_result_set()
         assert called_result.num_eat == 2
         assert called_result.num_bite == 0
 
-    def test_call_1eat0bite(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([1, 4, 5])
+    def test_call_1eat0bite(self, fixed_target) -> None:
+        called_result = Called("145", fixed_target).get_result_set()
         assert called_result.num_eat == 1
         assert called_result.num_bite == 0
 
-    def test_call_1eat1bite(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([1, 4, 2])
+    def test_call_1eat1bite(self, fixed_target) -> None:
+        called_result = Called("142", fixed_target).get_result_set()
         assert called_result.num_eat == 1
         assert called_result.num_bite == 1
 
-    def test_call_1eat2bite(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([1, 3, 2])
+    def test_call_1eat2bite(self, fixed_target) -> None:
+        called_result = Called("132", fixed_target).get_result_set()
         assert called_result.num_eat == 1
         assert called_result.num_bite == 2
 
-    def test_call_0eat0bite(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([4, 5, 6])
+    def test_call_0eat0bite(self, fixed_target) -> None:
+        called_result = Called("456", fixed_target).get_result_set()
         assert called_result.num_eat == 0
         assert called_result.num_bite == 0
 
-    def test_call_0eat1bite(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([4, 5, 1])
+    def test_call_0eat1bite(self, fixed_target) -> None:
+        called_result = Called("451", fixed_target).get_result_set()
         assert called_result.num_eat == 0
         assert called_result.num_bite == 1
 
-    def test_call_0eat2bite(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([3, 5, 1])
+    def test_call_0eat2bite(self, fixed_target) -> None:
+        called_result = Called("351", fixed_target).get_result_set()
         assert called_result.num_eat == 0
         assert called_result.num_bite == 2
 
-    def test_call_3bite(self, fixed_moo) -> None:
-        called_result = fixed_moo.call([2, 3, 1])
+    def test_call_3bite(self, fixed_target) -> None:
+        called_result = Called("231", fixed_target).get_result_set()
         assert called_result.num_eat == 0
         assert called_result.num_bite == 3
