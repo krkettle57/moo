7e82c2a67aec625be30b93ace648002604c6a881
krkettle57 <krkettle57@gmail.com>
2021-10-05 16:48:46 +0900

    add description

--- src/cli.py
+++ src/cli.py
@@ -8,51 +8,91 @@ from viewer import MOOCLIViewer
 
 @dataclass
 class MOOCLIHandler:
-    viewer: MOOCLIViewer = field(default=MOOCLIViewer(), init=False)
-    store: MOOFileStore = MOOJSONStore()
+    """CLI版 MOO"""
+
+    __viewer: MOOCLIViewer = field(default=MOOCLIViewer(), init=False)
+    __store: MOOFileStore = MOOJSONStore()
 
     def start(self, target_length: TargetLengthOption = 3) -> str:
+        """MOOを開始します
+
+        Parameters
+        ----------
+        target_length : TargetLengthOption, optional
+            ターゲットの桁数
+
+        Returns
+        -------
+        str
+            出力メッセージ
+        """
         if self._is_on_play():
-            yn = input(self.viewer.start_verify())
+            yn = input(self.__viewer.start_verify())
             if yn.lower().strip() != "y":
-                return self.viewer.start_done()
+                return self.__viewer.start_done()
 
         target = Target.generate(target_length)
         moo = MOO(target)
-        self.store.save(moo)
-        return self.viewer.start_done()
+        self.__store.save(moo)
+        return self.__viewer.start_done()
 
     def call(self, called: Union[str, int]) -> str:
-        if not self._is_on_play():
-            return self.viewer.no_moo_on_play()
+        """ターゲットと思われる数字列をコールします
 
-        moo = self.store.load()
+        Parameters
+        ----------
+        called : Union[str, int]
+            コールする数字列
+
+        Returns
+        -------
+        str
+            出力メッセージ
+        """
+        if not self._is_on_play():
+            return self.__viewer.no_moo_on_play()
+
+        moo = self.__store.load()
         result = moo.call(str(called))
-        self.store.save(moo)
+        self.__store.save(moo)
 
         if result.num_eat == moo.target.length:
-            return self.viewer.clear(moo)
+            return self.__viewer.clear(moo)
 
-        return self.viewer.called_result(result)
+        return self.__viewer.called_result(result)
 
     def giveup(self) -> str:
-        if not self._is_on_play():
-            return self.viewer.no_moo_on_play()
+        """MOOを終了し、ターゲットを表示します
 
-        moo = self.store.load()
+        Returns
+        -------
+        str
+            出力メッセージ
+        """
+        if not self._is_on_play():
+            return self.__viewer.no_moo_on_play()
+
+        moo = self.__store.load()
         moo.finish()
-        self.store.save(moo)
-        return self.viewer.giveup(moo.target)
+        self.__store.save(moo)
+        return self.__viewer.giveup(moo.target)
 
     def history(self) -> str:
-        if not self._is_started():
-            return self.viewer.no_moo_started()
+        """コール結果一覧を表示します
 
-        moo = self.store.load()
-        return self.viewer.history(moo.called_results)
+        Returns
+        -------
+        str
+            出力メッセージ
+        """
+        if not self._is_started():
+            return self.__viewer.no_moo_started()
+
+        moo = self.__store.load()
+        return self.__viewer.history(moo.called_results)
 
     def _is_started(self) -> bool:
-        return self.store.exists()
+        return self.__store.exists()
 
     def _is_on_play(self) -> bool:
-        return self._is_started() and self.store.load().on_play
+        return self._is_started() and self.__store.load().on_play
