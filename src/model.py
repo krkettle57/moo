01f99bec75f561b8efaf82e792e9dd9d98f58562
krkettle57 <krkettle57@gmail.com>
2021-10-05 11:37:01 +0900

    add apply Called & Target to MOO

--- src/model.py
+++ src/model.py
@@ -58,29 +58,16 @@ class Called:
 
 @dataclass
 class MOO:
-    target_length: TargetLengthOption
-    target: List[int] = field(default_factory=list)
+    target: Target
     called_results: List[CallResultSet] = field(default_factory=list)
-    onPlay: bool = field(default=True)
+    on_play: bool = field(default=True)
 
-    def __post_init__(self) -> None:
-        if self.target_length not in get_args(TargetLengthOption):
-            raise ValueError(f"'target_length' should be in {get_args(TargetLengthOption)}.")
-
-        if len(self.target) == 0:
-            self.target = [randint(0, 9) for _ in range(self.target_length)]
-
-    def call(self, called: List[int]) -> CallResultSet:
-        if len(called) != self.target_length:
-            raise ValueError(f"Length of 'called' must be {self.target_length}")
-
-        num_eat = sum([td == cd for td, cd in zip(self.target, called)])
-        num_bite = len(set(self.target) & set(called)) - num_eat
-
-        result = CallResultSet("".join([str(i) for i in called]), num_eat, num_bite)
+    def call(self, digits: str) -> CallResultSet:
+        called = Called(digits, self.target)
+        result = called.get_result_set()
         self.called_results.append(result)
 
         return result
 
     def finish(self) -> None:
-        self.onPlay = False
+        self.on_play = False
